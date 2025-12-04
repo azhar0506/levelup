e92c6388bfbc717ae592f7dc264fff28f89c8ec6
Azhar Basheer <azharbasheer0506@outlook.com>
2025-12-04 18:18:45 +0000

    2nd iteration of analyze logs project - i need more practice

--- Shell/projects/log_analyser/analyse_logs.sh
+++ Shell/projects/log_analyser/analyse_logs.sh
@@ -3,22 +3,29 @@
 set -euo pipefail
 
 if [ $# -ne 1 ]; then
-    echo "Enter 1 log file"
-elif [ "$1" != "logs.txt" ]; then
-    echo "Enter the correct log file"
+    echo "Enter 1 log file" >&2
+    exit 1
+elif [ ! -f "$1" ]; then
+    echo "File does not exist: $1" >&2
+    exit 1
 fi
 
 echo " ####### TOP 5 IPs ########### "
 
-cut -d " " -f 1 logs.txt | sort | uniq -c | sort -nr | head -5
+# cut -d " " -f 1 $1 | sort | uniq -c | sort -nr | head -5
+
+awk '{print $1}' "$1" | sort | uniq -c | sort -nr | head -5
 
 echo " ####### number of 2xx / 3xx / 4xx / 5xx #######"
 
-awk '{print $9}' logs.txt | sort | uniq -c | sort -nr | head -5
+echo "2xx: $(awk '{print $9}' "$1" | grep '^2' | wc -l)"
+echo "3xx: $(awk '{print $9}' "$1" | grep '^3' | wc -l)"
+echo "4xx: $(awk '{print $9}' "$1" | grep '^4' | wc -l)"
+echo "5xx: $(awk '{print $9}' "$1" | grep '^5' | wc -l)"
 
 echo "  ####### TOP 5 url ###########"
 
-awk '{print $7 $8}' logs.txt | sort | uniq -c | sort -nr | head -5
+awk '{print $7}' "$1" | sort | uniq -c | sort -nr | head -5
 
 
 
